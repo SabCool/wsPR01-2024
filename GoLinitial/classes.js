49ebc98ecfbfe8f1bcc3ced29cd887a236605da0
SabCool <sabine.classnitz@tumo.center>
2024-01-16 19:51:33 +0100

    inheritance

--- GoLinitial/classes.js
+++ GoLinitial/classes.js
@@ -1,4 +1,37 @@
-class Grass {
+class LivingCreature{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    findFields(symbol) {
+        let found = [];
+        for (let i = 0; i < this.neighbors.length; i++) {
+            const pos = this.neighbors[i]; // [x, y]
+            let posX = pos[0];
+            let posY = pos[1];
+            if (posX >= 0 && posX < matrix[0].length &&
+                posY >= 0 && posY < matrix.length) {
+                if (matrix[posY][posX] == symbol) {
+                    found.push(pos);
+                }
+            }
+
+        }
+        return found;
+    }
+}
+class Grass extends LivingCreature {
     constructor(x, y) {
         // Farbe - grün
         this.colorValue = 1;
