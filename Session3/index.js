2ff1b8ebb43018c2a5b560102c2303482a203cee
SabCool <sabine.classnitz@tumo.center>
2024-01-25 17:07:23 +0100

    session 6 eigene module

---
+++ Session3/index.js
@@ -0,0 +1,8 @@
+const Square = require("./square.js");
+
+let sq = new Square(10);
+let area = sq.getArea();
+console.log(sq.side);
+console.log(area);
+
+let sq2 = new Square(20);
--- myWebserver/server.js
+++ myWebserver/server.js
@@ -1,29 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('../GoLEdit/'))
+app.use(express.static("../GoLEdit/"));
 
-app.get("/", function(req, res){
-   res.send("<h1>Hello world</h1><p>lalala</p>");
+app.get("/", function (req, res) {
+  res.send("<h1>Hello world</h1><p>lalala</p>");
 });
 
-app.get("/google", function(req, res){
-    res.redirect('https://google.com');
+app.get("/google", function (req, res) {
+  res.redirect("https://google.com");
 });
 
-app.get("/search/:search", function(req, res){
-    const searchTerm = req.params.search;
-    res.redirect('https://google.com/search?q='+searchTerm);
+app.get("/search/:search", function (req, res) {
+  const searchTerm = req.params.search;
+  res.redirect("https://google.com/search?q=" + searchTerm);
 });
 
-app.get("/game", function(req,res){
-    res.redirect("game.html");
-})
+app.get("/game", function (req, res) {
+  res.redirect("game.html");
+});
 
-app.get("/*", function(req, res){
-    res.status(404).send("<p>Fehler 404: Datei nicht vorhanden...</p>");
-})
+app.get("/*", function (req, res) {
+  res.status(404).send("<p>Fehler 404: Gibts nicht vorhanden...</p>");
+});
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
+app.listen(3000, function () {
+  console.log("Example is running on port 3000");
 });
