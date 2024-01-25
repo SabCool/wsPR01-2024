9ab95a62f6433e65e1296fde922b6a28223c9c8a
SCL <sabine.classnitz@htw-berlin.de>
2024-01-25 09:26:12 +0100

    update express server mit Spielroute

--- myWebserver/server.js
+++ myWebserver/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('../GoLEdit/'))
+
 app.get("/", function(req, res){
    res.send("<h1>Hello world</h1><p>lalala</p>");
 });
@@ -13,6 +15,14 @@ app.get("/search/:search", function(req, res){
     const searchTerm = req.params.search;
     res.redirect('https://google.com/search?q='+searchTerm);
 });
+
+app.get("/game", function(req,res){
+    res.redirect("game.html");
+})
+
+app.get("/*", function(req, res){
+    res.status(404).send("<p>Fehler 404: Datei nicht vorhanden...</p>");
+})
 
 app.listen(3000, function(){
    console.log("Example is running on port 3000");
